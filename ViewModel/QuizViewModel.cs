ea5a69a7db6725ae63c27f7d910ac9abf78fa38d
letranphu <52100572@student.tdtu.edu.vn>
2023-10-25 21:17:44 +0700

    finish create quiz

--- ViewModel/QuizViewModel.cs
+++ ViewModel/QuizViewModel.cs
@@ -3,6 +3,8 @@
     public class QuizViewModel
     {
         public string QuizTitle { get; set; }
+        public bool QuizPublished { get; set; }
+        public int QuizLevel { get; set; }
         public List<QuestionViewModel> Questions { get; set; }
     }
 }
--- wwwroot/js/createquiz.js
+++ wwwroot/js/createquiz.js
@@ -104,6 +104,34 @@
         $(this).closest('.create-quiz-card').remove();
     });
 
+    $("#level-select select").change(function () {
+        // Get the selected option value
+        var selectedValue = $(this).val();
+        var circle = $(".circle");
+
+        // Remove existing color classes
+        $(this).removeClass("text-success text-warning text-danger");
+        circle.removeClass("bg-success bg-warning bg-danger");
+
+        // Add the appropriate color class based on the selected option
+        switch (selectedValue) {
+            case "1":
+                $(this).addClass("text-success");
+                circle.addClass("bg-success");
+                break;
+            case "2":
+                $(this).addClass("text-warning");
+                circle.addClass("bg-warning");
+                break;
+            case "3":
+                $(this).addClass("text-danger");
+                circle.addClass("bg-danger");
+                break;
+            default:
+            // If none of the above, you can add a default class here
+        }
+    });
+
     // Handle form submission
     $("#submit").click(function () {
         var quizTitle = $("#quizTitle").val();
