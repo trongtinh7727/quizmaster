b2b7d0f7849d4ab91675574bbebbe3b5638b0b1d
letranphu <52100572@student.tdtu.edu.vn>
2023-11-08 17:04:49 +0700

    quiz results: sort, search

--- wwwroot/js/quizresults.js
+++ wwwroot/js/quizresults.js
@@ -1,12 +1,3 @@
 ﻿$(document).ready(function () {
-    var tagClasses = ['left-tag-1', 'left-tag-2', 'left-tag-3'];
-    var currentIndex = 0;
-
-    $('.results-container-item').each(function (index) {
-        // Add the class based on the currentIndex
-        $(this).addClass(tagClasses[currentIndex]);
-
-        // Increment the currentIndex and reset if it reaches the end of the tagClasses array
-        currentIndex = (currentIndex + 1) % tagClasses.length;
-    });
+    new DataTable('#results-container');
 });
