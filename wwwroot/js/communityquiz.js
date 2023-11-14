3a06b8799e480b21395ad4d990b3ba44f4214908
letranphu <52100572@student.tdtu.edu.vn>
2023-11-14 21:16:51 +0700

    leaderboard, edit warning, home page limit 8

--- wwwroot/js/communityquiz.js
+++ wwwroot/js/communityquiz.js
@@ -44,4 +44,18 @@
             }
         });
     });
+
+    // When the Edit button is clicked in the detailModal
+    $('[data-bs-target^="#rankingModal"]').on('click', function () {
+        var targetModalId = $(this).data('bs-target');
+        var detailModalId = $(this).closest('.modal').attr('id');
+
+        // Hide the detailModal
+        $('#' + detailModalId).modal('hide');
+
+        // Show the rankingModal after a short delay to allow transition
+        setTimeout(function () {
+            $(targetModalId).modal('show');
+        }, 500); // Adjust this timeout as needed
+    });
 })
