d783872bbdf82befd4725f839b0f6c9374b8e043
letranphu <52100572@student.tdtu.edu.vn>
2023-11-17 21:24:46 +0700

    feat: library -> quiz delete button -> modal

--- wwwroot/js/library.js
+++ wwwroot/js/library.js
@@ -35,4 +35,18 @@
             $(targetModalId).modal('show');
         }, 500); // Adjust this timeout as needed
     });
+
+    // When the Delete button is clicked in the detailModal
+    $('[data-bs-target^="#deleteWarningModal"]').on('click', function () {
+        var targetModalId = $(this).data('bs-target');
+        var detailModalId = $(this).closest('.modal').attr('id');
+
+        // Hide the detailModal
+        $('#' + detailModalId).modal('hide');
+
+        // Show the deleteWarningModal after a short delay to allow transition
+        setTimeout(function () {
+            $(targetModalId).modal('show');
+        }, 500); // Adjust this timeout as needed
+    });
 });
