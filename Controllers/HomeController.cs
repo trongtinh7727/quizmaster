ae7a73661dbae95145364049235cc33659c0fc90
letranphu <52100572@student.tdtu.edu.vn>
2023-11-24 22:29:43 +0700

    ui: celebrate

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -133,5 +133,9 @@ namespace QuizMaster.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Celebrate()
+        {
+            return View();
+        }
     }
 }
